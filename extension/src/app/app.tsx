9889b00fc59fe0e67354699cd8a184886a3a621b
Zarazaka Mikalai <golmtz@gmail.com>
2023-11-02 13:45:51 +0300

    add browse extension

--- extension/src/app/app.tsx
+++ extension/src/app/app.tsx
@@ -1,9 +1,10 @@
-import { HomePage } from "@/pages/home.page.tsx";
+import { AppProvider } from "@/app/app-providers.tsx";
+import { AppRouter } from "@/app/app-router.tsx";
 
 export function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <AppProvider>
+      <AppRouter />
+    </AppProvider>
   );
 }
